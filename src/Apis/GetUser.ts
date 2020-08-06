3bf0b30e395efde04f940c58a6b829efdd24d4a8
s00d <Virus191288@gmail.com>
2020-08-06 13:48:34 +0500

    fix types

--- src/Apis/GetUser.ts
+++ src/Apis/GetUser.ts
@@ -15,8 +15,7 @@ interface User {
   verify: number;
   block: number;
   payment: {
-    balance: number,
-    zbalance: number,
+    payment: number,
     income: number,
     spent: number,
     now: number,
