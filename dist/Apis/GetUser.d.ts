6281f38ee9f6f644555bf31069155f2deb8a6d4d
s00d <Virus191288@gmail.com>
2021-08-17 16:02:41 +0500

    fix path

--- dist/Apis/GetUser.d.ts
+++ dist/Apis/GetUser.d.ts
@@ -20,6 +20,78 @@ export interface User {
         now: number;
     };
 }
+export interface PayList {
+    forms: {
+        [key: number]: string;
+    };
+    currency: {
+        [key: string]: number;
+    };
+    orders: {
+        current_page: number;
+        first_page_url: string;
+        from: number;
+        last_page: number;
+        last_page_url: string;
+        data: {
+            [key: string]: {
+                id: number;
+                id_user: number;
+                stat: 0 | 1 | 2;
+                sum: string;
+                cashback: number;
+                form: number;
+                id_sale: string | null;
+                promo: null | string;
+                original_sum: number;
+                currency: string;
+                pay_at: null | string;
+                created_at: string;
+                updated_at: string;
+            };
+        };
+    };
+    paylist: {
+        [key: string]: {
+            list_id: number;
+            enable: boolean;
+            id: number;
+            name: string;
+            title: string;
+            image: string;
+            invert: boolean;
+            add: {
+                [key: string]: string;
+            };
+            coofPersent: number;
+            curr: {
+                RUB: boolean;
+                USD: boolean;
+                CNY: boolean;
+                EUR: boolean;
+                cryptocurrency: boolean;
+            };
+            links_method?: string;
+            links?: Array<{
+                sum: number;
+                id: string;
+                classes: string;
+            }>;
+        };
+    };
+}
+export interface Pay {
+    payId: number;
+    params: {
+        url: string;
+        label: string;
+        sum: string;
+        comment: string;
+        newtab: boolean;
+        noreferrer: boolean;
+        [key: string]: string | number | boolean;
+    };
+}
 export default class GetUser extends _base {
     balance(): Promise<{
         balance: number;
@@ -27,4 +99,8 @@ export default class GetUser extends _base {
         income: number;
     }>;
     profile(): Promise<User>;
+    getPaymentHistory(): Promise<PayList>;
+    createEmpty(params: {
+        [key: string]: any;
+    }): Promise<Pay>;
 }
