8f81de8aa8793fd564fa5b4149faef868d43d573
s00d <Virus191288@gmail.com>
2021-08-17 13:20:46 +0500

    add payment api

--- src/Apis/GetUser.ts
+++ src/Apis/GetUser.ts
@@ -22,6 +22,74 @@ export interface User {
   };
 }
 
+export interface PayList {
+  forms: {[key: number]: string},
+  currency: {[key: string]: number},
+  orders: {
+    current_page: number,
+    first_page_url: string,
+    from: number,
+    last_page: number,
+    last_page_url: string,
+    data: {[key: string]: {
+        id: number,
+        id_user: number,
+        stat: 0|1|2,
+        sum: string,
+        cashback: number,
+        form: number,
+        id_sale: string|null,
+        promo: null|string,
+        original_sum: number,
+        currency: string,
+        pay_at: null|string,
+        created_at: string,
+        updated_at: string,
+      }
+    }
+  },
+  paylist: {[key: string]: {
+      list_id: number,
+      enable: boolean,
+      id: number,
+      name: string,
+      title: string,
+      image: string,
+      invert: boolean,
+      add: {
+        [key: string]: string
+      },
+      coofPersent: number,
+      curr: {
+        RUB: boolean,
+        USD: boolean,
+        CNY: boolean,
+        EUR: boolean,
+        cryptocurrency: boolean,
+      },
+      links_method?: string,
+      links?: Array<{
+        sum: number,
+        id: string,
+        classes: string,
+      }>
+    }
+  }
+}
+
+export interface Pay {
+  payId: number,
+  params: {
+    url: string,
+    label: string,
+    sum: string,
+    comment: string,
+    newtab: boolean,
+    noreferrer: boolean,
+    [key: string]: string|number|boolean
+  },
+}
+
 export default class GetUser extends _base {
   balance(): Promise<{ balance: number, zbalance: number, income: number }> {
     return this.getRequest('getBalance', {income: true}).then((res) => {
@@ -36,6 +104,18 @@ export default class GetUser extends _base {
   profile(): Promise<User> {
     return this.getRequest('getProfile', {income: true}).then((res) => {
       return res.profile
+    })
+  }
+
+  getPaymentHistory(): Promise<PayList> {
+    return this.getRequest('getPaymentHistory').then((res) => {
+      return res
+    })
+  }
+
+  createEmpty(currency: string, form: number, sum: number): Promise<Pay> {
+    return this.getRequest('createEmpty', {currency, form, sum}, false).then((res) => {
+      return res
     })
   }
 }
